8d6a5ddbaf1f75b9119090113191f2dd57857a5a
Christopher Donofrio <christopherdonofrio@Christophers-MacBook-Pro.local>
2020-07-28 15:10:46 -0400

    added avatar component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -5,8 +5,18 @@ import './index.css';
 function Tweet()	{
 	return (
 		<div className="tweet">
+			<Avatar/>
 			Tweet
 		</div>
+	);
+}
+
+function Avatar() {
+	return (
+		<img
+			src="https://www.gravatar.com/avatar/nothing"
+			className="avatar"
+			alt="avatar" />
 	);
 }
 
