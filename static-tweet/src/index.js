584960170264e57af1836b5c553a73ede36d3f3a
Christopher Donofrio <christopherdonofrio@Christophers-MacBook-Pro.local>
2020-07-30 13:19:04 -0400

    added button components, finished static tweet

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -7,8 +7,14 @@ function Tweet()	{
 		<div className="tweet">
 			<Avatar/>
 			<div className="content">
-				<Author/>
+				<Author/><Time/>
 				<Message/>
+				<div className="buttons">
+					<ReplyButton/>
+					<RetweetButton/>
+					<LikeButton/>
+					<MoreOptionsButton/>
+				</div>
 			</div>
 		</div>
 	);
@@ -39,6 +45,26 @@ function Author() {
 		</span>
 	);
 }
+
+const Time = () => (
+	<span className="time">3h ago</span>
+);
+
+const ReplyButton = () => (
+	<i className="fa fa-reply reply-button"/>
+);
+
+const RetweetButton = () => (
+	<i className="fa fa-retweet retweet-button"/>
+);
+
+const LikeButton = () => (
+	<i className="fa fa-heart like-button"/>
+);
+
+const MoreOptionsButton = () => (
+	<i className="fa fa-ellipsis-h more-options-button"/>
+);
 
 ReactDOM.render(<Tweet/>,
 	document.querySelector('#root'));
